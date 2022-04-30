ea740b43510130c9e1959e00ba9b362b6716a1d8
VitaliyRom <romanovvit04@gmail.com>
2022-04-30 19:33:51 +0700

    2.1.3

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -5,7 +5,10 @@ import app.model.AnimalsCage;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+
 public class Application {
+
+
 
     public static void main(String[] args) {
         ApplicationContext applicationContext =
